7b3103efba5b2edb21759d8df48234f110c74f41
Daniel Santin <86211080+DanielSantin1@users.noreply.github.com>
2024-01-24 13:52:38 -0300

    24012024

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function greeting(name) {
     console.log("Olá" + name);
 }
 greeting(firtsName);
+const a = 5
+const b = 5
+const c = a + b
+
+function soma(c){
+console.log('Soma é: '+c)
+}
+soma(c)
--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 const firtsName = "Daniel"
 const anotherName = 1
 const x = true
+
 function greeting (name: string){
     console.log("Olá"+name)
 }
 greeting(firtsName)
+const a = 5
+const b = 5
+const c = a + b
 
+function soma(c: Number){
+console.log('Soma é: '+c)
+}
+soma(c)
 
  
