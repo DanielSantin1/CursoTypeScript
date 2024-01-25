43d17d43ddfe9ecc30ba23e031ae3e7e3a164106
Daniel Santin <86211080+DanielSantin1@users.noreply.github.com>
2024-01-25 13:30:06 -0300

    250012024

---
+++ Fundamentos/index.js
@@ -0,0 +1,23 @@
+// 1 - numbers
+var x = 10;
+console.log(x);
+x = 16;
+console.log(typeof x);
+var y = 489789.4548;
+console.log(typeof y);
+console.log(y);
+console.log(y.toPrecision(3));
+//2-- string
+var firstName = "Daniel";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Santin";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+// 3 - Boolean
+var a = false;
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
---
+++ Fundamentos/index.ts
@@ -0,0 +1,51 @@
+// 1 - numbers
+
+let x: number = 10
+
+console.log(x)
+
+x = 16
+
+console.log(typeof x)
+
+const y: number = 489789.4548
+
+console.log(typeof y)
+
+console.log(y)
+
+console.log(y.toPrecision(3))
+
+//2-- string
+
+const firstName: string = "Daniel"
+
+console.log(firstName.toUpperCase())
+
+let fullName: string 
+const lastName: string = "Santin"
+
+fullName = firstName + " " + lastName
+console.log(fullName)
+console.log(typeof fullName)
+
+// 3 - Boolean
+
+let a: boolean = false
+
+console.log(a)
+console.log(typeof a)
+
+a = true
+
+
+console.log(a)
+
+//4-- inference e annotation
+
+const ann: string = "Teste"
+let inf = "Teste"
+
+// ann = 1
+// inf = 1
+
