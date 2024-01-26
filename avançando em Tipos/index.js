8ccc294b93c352f9d17a952cbba0c661d90791f8
Daniel Santin <86211080+DanielSantin1@users.noreply.github.com>
2024-01-26 15:22:54 -0300

    26012024

---
+++ avançando em Tipos/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// 1 - arrays
+let numbers = [1, 2, 3];
+numbers.push(5);
+console.log(numbers[2]);
+// numbers = "teste"
+const nomes = ["Matheus", "João"];
+// nomes.push(2)
+//-- 2 outro sintaxe de array
+const nums = [100, 200];
+nums.push(300);
+// nums.push("teste")
+console.log(nums);
+console.log(nums[0]);
